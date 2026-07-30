b3af1e422373c72a03e63575d6d8e1166002c8a5
Chanwu_Oh <ohchanwu@gmail.com>
2026-07-30 14:08:53 +0900

    add User

--- src/com/ssafy/nyamnyamcoach/domain/User.java
+++ src/com/ssafy/nyamnyamcoach/domain/User.java
@@ -1,5 +1,8 @@
 package com.ssafy.nyamnyamcoach.domain;
 
+import java.util.*;
+import java.time.*;
+
 class User {
 	String id;
 	public String getId() { return id; }
@@ -8,4 +11,32 @@ class User {
 	String name;
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
+	
+	String password;
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+	
+	int height;
+	public int getHeight() { return height; }
+	public void setHeight(int height) { this.height = height; }
+	
+	int weight;
+	public int getWeight() { return weight; }
+	public void setWeight(int weight) { this.weight = weight; }
+	
+	List<String> diseases;
+	public List<String> getDiseases() { return diseases; }
+	public void setDiseases(List<String> diseases) { this.diseases = diseases; }
+	
+	String status;
+	public String getstatus() { return status; }
+	public void setstatus(String status) { this.status = status; }
+	
+	String followingIds;
+	public String getfollowingIds() { return followingIds; }
+	public void setfollowingIds(String followingIds) { this.followingIds = followingIds; }
+	
+	String createdAt;
+	public String getcreatedAt() { return createdAt; }
+	public void setcreatedAt(String createdAt) { this.createdAt = createdAt; }
 }
